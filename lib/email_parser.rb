041ed8dc77b61917dbbef7aefcd334ea092282c7
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-26 22:25:55 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,5 +10,11 @@ class EmailAddressParser
     @emails = emails
   end
   
-  
+   def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
+end 
 end
